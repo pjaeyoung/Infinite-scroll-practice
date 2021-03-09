44355def142d5b6a44b2456667fef25aa0ccd160
pjaeyoung <devdodose@gmail.com>
2021-03-09 16:45:49 +0900

    test: if valid arguments are passed, the InfiiniteScroll instance has a scrollList attribute

--- src/InfiniteScroll.test.js
+++ src/InfiniteScroll.test.js
@@ -32,5 +32,10 @@ describe("InfiniteScroll", () => {
         }).toThrowError(InfiniteScroll.messages.invalidConstructorArg);
       });
     });
+
+    it("올바른 인자로 넘기면 scrollList값이 속성으로 지정되어야 합니다.", () => {
+      const infiniteScroll = new InfiniteScroll(goodArgs);
+      expect(infiniteScroll.scrollList).toEqual(goodArgs.scrollList);
+    });
   });
 });
